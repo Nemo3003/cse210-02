3b7d826c56f5408f9cdd3b74e6dd271e7425d027
Ulises Mariano Melgarejo <ulimarian2000@gmail.com>
2022-01-28 08:38:34 -0300

    it worked!

--- W3HiLo.py
+++ W3HiLo.py
@@ -94,15 +94,18 @@ newcard = NextCard()
 nextCard = newcard.draw(deck)
 #The first value for the score is 300
 score = 300
+card = random.randint(1,13)
+nextCards = random.randint(1,13)
 
 play = input("Do you want to play? Y or N? ")
 while play.lower()== "y":
-    card = playerHand
-    print("\nThe current card is: ", (card.show()))
+    
+    
+    print("\nThe current card is: ", (card))
     guess = input("Guess H for high or L for low: " )
     if guess.lower()=="h":
-        if card > nextCard:
-            print(f"\nCorrect! The the card is {card.show()}")
+        if card > nextCards:
+            print(f"\nCorrect! The the card is {card}")
             score += 100
             # adding = Counter.append(score)
             print(f"Your points are {score}")
@@ -114,11 +117,10 @@ while play.lower()== "y":
                 if play_ag.lower()=="n":
                     print("Thanks for playing, hope you enjoyed!")
                     break
-            
-            
 
-        if card < nextCard:
-            print(f"\nYou lost! The card was {nextCard.show()}")
+
+        if card < nextCards:
+            print(f"\nYou lost! The card was {card}")
             score -= 75
             print(f"Your points are {score}")
             if score == 0 or score < 0:
@@ -129,11 +131,11 @@ while play.lower()== "y":
                 if play_ag.lower()=="n":
                     print("Thanks for playing, hope you enjoyed!")
                     break
-            
+
 
     if guess.lower()=="l":
-        if card > nextCard:
-            print(f"\nYou lost! card was {nextCard.show()}")
+        if card > nextCards:
+            print(f"\nYou lost! card was {nextCards}")
             score -= 75
             print(f"Your points are {score}")
             if score == 0 or score < 0:
@@ -144,10 +146,10 @@ while play.lower()== "y":
                 if play_ag.lower()=="n":
                     print("Thanks for playing, hope you enjoyed!")
                     break
-            
 
-        if card < nextCard:
-            print(f"\nCorrect! The the card is {nextCard.show()}")
+
+        if card < nextCards:
+            print(f"\nCorrect! The the card is {nextCards}")
             score += 100
             print(f"Your points are {score}")
             if score == 0 or score < 0:
@@ -158,7 +160,7 @@ while play.lower()== "y":
                 if play_ag.lower()=="n":
                     print("Thanks for playing, hope you enjoyed!")
                     break
-            
+
 
 else:
     print("Thanks for playing!")
