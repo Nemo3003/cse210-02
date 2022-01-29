889e27632e7d8923b9dc93bc4e0673466ff5d07c
Dan Allred <82766686+DanAllred@users.noreply.github.com>
2022-01-28 20:58:51 -0500

    commented out old classes not in use

--- W3HiLo.py
+++ W3HiLo.py
@@ -8,88 +8,88 @@ import random
 from typing import Counter
 # To create a python class, use the keyword 'class'
 # Use the __init__() function to assign values to object properties
-class Card(object):
-    def __init__(self, suit, value):
-        self.suit = suit
-        self.value = value
+# class Card(object):
+#     def __init__(self, suit, value):
+#         self.suit = suit
+#         self.value = value
 
-    def __gt__(self, comparing_card):
-        result = self.value > comparing_card.value
-        return result
+#     def __gt__(self, comparing_card):
+#         result = self.value > comparing_card.value
+#         return result
 
-    def __lt__(self, comparing_card):
-        result = self.value < comparing_card.value
-        return result
+#     def __lt__(self, comparing_card):
+#         result = self.value < comparing_card.value
+#         return result
 
-    def show(self):
-        print ("{} of {}".format(self.value, self.suit))
+#     def show(self):
+#         print ("{} of {}".format(self.value, self.suit))
 
 
-class Deck(object):
-    def __init__(self):
-        self.cards = []
-        self.build()
+# class Deck(object):
+#     def __init__(self):
+#         self.cards = []
+#         self.build()
 
-    def build(self):
-        for suit in ["Spades", "Clubs", "Diamonds", "Hearts"]:
-            for value in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"]:  #Ace, Jack, queen and King need comparible. or we could just name them 1, 11, 12, 13
-                self.cards.append(Card(suit, value))
+#     def build(self):
+#         for suit in ["Spades", "Clubs", "Diamonds", "Hearts"]:
+#             for value in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"]:  #Ace, Jack, queen and King need comparible. or we could just name them 1, 11, 12, 13
+#                 self.cards.append(Card(suit, value))
 
-    def show(self):
-        for card in self.cards:
-            card.show()
+#     def show(self):
+#         for card in self.cards:
+#             card.show()
 
-    def shuffle(self):
-        for n in range(len(self.cards)-1,0,-1):
-            r = random.randint(0, n)
-            self.cards[n], self.cards[r] = self.cards[r], self.cards[n]
+#     def shuffle(self):
+#         for n in range(len(self.cards)-1,0,-1):
+#             r = random.randint(0, n)
+#             self.cards[n], self.cards[r] = self.cards[r], self.cards[n]
 
-    def drawCard(self):
-        return self.cards.pop()
+#     def drawCard(self):
+#         return self.cards.pop()
 
-class Person(object):
-    def __init__(self):
-        self.hand = []
+# class Person(object):
+#     def __init__(self):
+#         self.hand = []
 
-    def __gt__(self, comparing_obj):
+#     def __gt__(self, comparing_obj):
         
-        result = self.hand[-1] > comparing_obj.nextcard[-1]
-        return result
+#         result = self.hand[-1] > comparing_obj.nextcard[-1]
+#         return result
 
-    def __lt__(self, comparing_obj):
+#     def __lt__(self, comparing_obj):
         
-        result = self.hand[-1] < comparing_obj.nextcard[-1]
-        return result
+#         result = self.hand[-1] < comparing_obj.nextcard[-1]
+#         return result
 
-    def draw(self, deck):
-        self.hand.append(deck.drawCard())
-        return self
+#     def draw(self, deck):
+#         self.hand.append(deck.drawCard())
+#         return self
 
-    def show(self):
-        for card in self.hand:
-            card.show()
+#     def show(self):
+#         for card in self.hand:
+#             card.show()
 
-class NextCard(object):
-    def __init__(self):
-        self.nextcard = []
+# class NextCard(object):
+#     def __init__(self):
+#         self.nextcard = []
 
-    def draw(self, deck):
-        self.nextcard.append(deck.drawCard())
-        return self
+#     def draw(self, deck):
+#         self.nextcard.append(deck.drawCard())
+#         return self
 
-    def show(self):
-        for card1 in self.nextcard:
-            card1.show()
+#     def show(self):
+#         for card1 in self.nextcard:
+#             card1.show()
 
 
 
-deck = Deck()
-deck.shuffle()
+# deck = Deck()
+# deck.shuffle()
 
-human = Person()
-playerHand = human.draw(deck)
-newcard = NextCard()
-nextCard = newcard.draw(deck)
+# human = Person()
+# playerHand = human.draw(deck)
+# newcard = NextCard()
+# nextCard = newcard.draw(deck)
 #The first value for the score is 300
 score = 300
 card = random.randint(1,13)
