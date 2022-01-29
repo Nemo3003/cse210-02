70e68787cadcdb4665ad9c5a235197a2f1aad408
Ulises Mariano Melgarejo <ulimarian2000@gmail.com>
2022-01-29 08:47:10 -0300

    a class added and try except

--- W3HiLo.py
+++ W3HiLo.py
@@ -6,157 +6,84 @@ Edit 1/22/2022 3:00pm EST. - GO - Class comments
 
 import random
 from typing import Counter
-# To create a python class, use the keyword 'class'
-# Use the __init__() function to assign values to object properties
-# class Card(object):
-#     def __init__(self, suit, value):
-#         self.suit = suit
-#         self.value = value
-
-#     def __gt__(self, comparing_card):
-#         result = self.value > comparing_card.value
-#         return result
-
-#     def __lt__(self, comparing_card):
-#         result = self.value < comparing_card.value
-#         return result
-
-#     def show(self):
-#         print ("{} of {}".format(self.value, self.suit))
 
 
-# class Deck(object):
-#     def __init__(self):
-#         self.cards = []
-#         self.build()
 
-#     def build(self):
-#         for suit in ["Spades", "Clubs", "Diamonds", "Hearts"]:
-#             for value in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"]:  #Ace, Jack, queen and King need comparible. or we could just name them 1, 11, 12, 13
-#                 self.cards.append(Card(suit, value))
 
-#     def show(self):
-#         for card in self.cards:
-#             card.show()
+class Game:
 
-#     def shuffle(self):
-#         for n in range(len(self.cards)-1,0,-1):
-#             r = random.randint(0, n)
-#             self.cards[n], self.cards[r] = self.cards[r], self.cards[n]
+    #TODO: To use at least one class.
 
-#     def drawCard(self):
-#         return self.cards.pop()
-
-# class Person(object):
-#     def __init__(self):
-#         self.hand = []
-
-#     def __gt__(self, comparing_obj):
+    def let_play():
+        try:
+            card = random.randint(1,13)
+            nextCards = random.randint(1,13)
+            score = 300
         
-#         result = self.hand[-1] > comparing_obj.nextcard[-1]
-#         return result
-
-#     def __lt__(self, comparing_obj):
-        
-#         result = self.hand[-1] < comparing_obj.nextcard[-1]
-#         return result
-
-#     def draw(self, deck):
-#         self.hand.append(deck.drawCard())
-#         return self
-
-#     def show(self):
-#         for card in self.hand:
-#             card.show()
-
-# class NextCard(object):
-#     def __init__(self):
-#         self.nextcard = []
-
-#     def draw(self, deck):
-#         self.nextcard.append(deck.drawCard())
-#         return self
-
-#     def show(self):
-#         for card1 in self.nextcard:
-#             card1.show()
+            play = input("Do you want to play? Y or N? ")
+            while play.lower()== "y":
+                print("\nThe current card is: ", (card))
+                guess = input("Guess H for high or L for low: " )
+                if guess.lower()=="h":
+                    if card < nextCards:
+                        print(f"\nCorrect! The the card is {nextCards}")
+                        score += 100
+                        # adding = Counter.append(score)
+                        print(f"Your points are {score}")
+                        if score == 0 or score < 0:
+                            print("You lost all your points, you lose!")
+                            break
+                        elif score > 0:
+                            play_ag = input("Play again? Y or N? ")
+                            if play_ag.lower()=="n":
+                                print("Thanks for playing, hope you enjoyed!")
+                                break
 
 
-
-# deck = Deck()
-# deck.shuffle()
-
-# human = Person()
-# playerHand = human.draw(deck)
-# newcard = NextCard()
-# nextCard = newcard.draw(deck)
-#The first value for the score is 300
-score = 300
-card = random.randint(1,13)
-nextCards = random.randint(1,13)
-#TODO: To use at least one class.
-play = input("Do you want to play? Y or N? ")
-while play.lower()== "y":
-    print("\nThe current card is: ", (card))
-    guess = input("Guess H for high or L for low: " )
-    if guess.lower()=="h":
-        if card > nextCards:
-            print(f"\nCorrect! The the card is {nextCards}")
-            score += 100
-            # adding = Counter.append(score)
-            print(f"Your points are {score}")
-            if score == 0 or score < 0:
-                print("You lost all your points, you lose!")
-                break
-            elif score > 0:
-                play_ag = input("Play again? Y or N? ")
-                if play_ag.lower()=="n":
-                    print("Thanks for playing, hope you enjoyed!")
-                    break
+                        print(f"\nYou lost! The card was {nextCards}")
+                    if card > nextCards:
+                        score -=75
+                        print(f"Your points are {score}")
+                        if score == 0 or score < 0:
+                            print("You lost all your points, you lose!")
+                            break
+                        elif score > 0:
+                            play_ag = input("Play again? Y or N? ")
+                            if play_ag.lower()=="n":
+                                print("Thanks for playing, hope you enjoyed!")
+                                break
 
 
-        if card < nextCards:
-            print(f"\nYou lost! The card was {nextCards}")
-            score -= 75
-            print(f"Your points are {score}")
-            if score == 0 or score < 0:
-                print("You lost all your points, you lose!")
-                break
-            elif score > 0:
-                play_ag = input("Play again? Y or N? ")
-                if play_ag.lower()=="n":
-                    print("Thanks for playing, hope you enjoyed!")
-                    break
+                if guess.lower()=="l":
+                    if card < nextCards:
+                        print(f"\nYou lost! card was {nextCards}")
+                        score += -75
+                        print(f"Your points are {score}")
+                        if score == 0 or score < 0:
+                            print("You lost all your points, you lose!")
+                            break
+                        elif score > 0:
+                            play_ag = input("Play again? Y or N? ")
+                            if play_ag.lower()=="n":
+                                print("Thanks for playing, hope you enjoyed!")
+                                break
 
 
-    if guess.lower()=="l":
-        if card > nextCards:
-            print(f"\nYou lost! card was {nextCards}")
-            score -= 75
-            print(f"Your points are {score}")
-            if score == 0 or score < 0:
-                print("You lost all your points, you lose!")
-                break
-            elif score > 0:
-                play_ag = input("Play again? Y or N? ")
-                if play_ag.lower()=="n":
-                    print("Thanks for playing, hope you enjoyed!")
-                    break
+                        print(f"\nCorrect! The card was {nextCards}")
+                    if card > nextCards:
+                        score += 100
+                        print(f"Your points are {score}")
+                        if score == 0 or score < 0:
+                            print("You lost all your points, you lose!")
+                            break
+                        elif score > 0:
+                            play_ag = input("Play again? Y or N? ")
+                            if play_ag.lower()=="n":
+                                print("Thanks for playing, hope you enjoyed!")
+                                break
 
-
-        if card < nextCards:
-            print(f"\nCorrect! The the card is {nextCards}")
-            score += 100
-            print(f"Your points are {score}")
-            if score == 0 or score < 0:
-                print("You lost all your points, you lose!")
-                break
-            elif score > 0:
-                play_ag = input("Play again? Y or N? ")
-                if play_ag.lower()=="n":
-                    print("Thanks for playing, hope you enjoyed!")
-                    break
-
-
-else:
-    print("Thanks for playing!")
+            else:
+                print("Thanks for playing!")
+        except ValueError:
+            print("Please enter a valid input")
+Game.let_play()
